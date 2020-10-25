c5f339158c4eb62f56a3f96a0c73646c7e5a8ba4
irtizak <irtizakaleem@gmail.com>
2020-10-25 19:35:32 -0400

    Update app file to have running model

--- ml_api/api/app.py
+++ ml_api/api/app.py
@@ -1,12 +1,58 @@
-from flask import Flask
+# Flask Extensions
+from flask import Flask, jsonify
+from flask_restful import Resource, Api, reqparse
 
-def create_app() -> Flask:
-    '''Create a flask app instance.'''
+# pydata stack
+import numpy as np
+import joblib
 
-    flask_app = Flask('ml_api')
+# set up Flask and Flask-Restful
+app = Flask(__name__)
+api = Api(app)
 
-    # import blueprints
-    from api.controller import prediction_app
-    flask_app.register_blueprint(prediction_app)
+# argument parsing
+parser = reqparse.RequestParser()
+parser.add_argument('pclass', type=float)
+parser.add_argument('age', type=float)
+# parser.add_argument('petal_length', type=float)
+# parser.add_argument('petal_width', type=float)
 
-    return flask_app
+
+class TitanicPredict(Resource):
+    def get(self):
+        # parse arguments
+        args = parser.parse_args()
+        pclass = args['pclass']
+        age = args['age']
+        # petal_length = args['petal_length']
+        # petal_width = args['petal_width']
+
+        # create numpy ndarray
+        pt_to_predict = np.array([[
+            pclass,
+            age
+            # petal_length,
+            # petal_width
+        ]])
+
+        # load model and predict
+        model = joblib.load('model/model.joblib')
+        predicted_class = model.predict(pt_to_predict)
+
+        # return answer
+        prediction = {
+            'predicted_class': predicted_class.tolist()
+        }
+        result = {'data': dict(prediction)}
+        return jsonify(result)
+
+
+# create endpoint
+api.add_resource(TitanicPredict, '/predict/titanic/')
+
+
+if __name__ == '__main__':
+    app.run(
+        host='127.0.0.1',
+        port='5000',
+    )
